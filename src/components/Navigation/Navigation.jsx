97cfc032b626393871cdfd3a9836b730be8a7120
Karol Jose Gutierrez Suarez <karol.gutierrez@kueski.com>
2018-10-12 17:49:10 -0500

    Add background and logo

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navigation = () =>{
+  return(
+    <nav style={{display: 'flex', justifyContent:'flex-end'}}>
+      <p className='f3 link dim black underline p13 pointer'>Sign out!</p>
+    </nav>
+  );
+}
+export default Navigation;
