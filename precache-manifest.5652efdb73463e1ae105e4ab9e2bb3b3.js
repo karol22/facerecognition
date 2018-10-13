b77ec6749d57c046ee06bd168b3407a959962522
Karol Jose Gutierrez Suarez <karol.gutierrez@kueski.com>
2018-10-13 15:27:58 -0500

    Updates

---
+++ precache-manifest.5652efdb73463e1ae105e4ab9e2bb3b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a73abdcfa4072db5abd",
+    "url": "/facerecognition/static/js/runtime~main.1a73abdc.js"
+  },
+  {
+    "revision": "bab44a8ce38afaab4c8b",
+    "url": "/facerecognition/static/js/main.bab44a8c.chunk.js"
+  },
+  {
+    "revision": "400d3e9b714ba56c4aee",
+    "url": "/facerecognition/static/js/1.400d3e9b.chunk.js"
+  },
+  {
+    "revision": "bab44a8ce38afaab4c8b",
+    "url": "/facerecognition/static/css/main.1e23e6b5.chunk.css"
+  },
+  {
+    "revision": "400d3e9b714ba56c4aee",
+    "url": "/facerecognition/static/css/1.bd52e46d.chunk.css"
+  },
+  {
+    "revision": "106f61340304b7afc4c1e4abc3a3f9da",
+    "url": "/facerecognition/index.html"
+  }
+];
